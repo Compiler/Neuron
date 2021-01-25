35aff2fd60d95538439092d8a0f75b18b317aaf2
compiler <lukerochedev@gmail.com>
2021-01-25 08:16:05 -0500

    layered N

--- core.py
+++ core.py
@@ -1,9 +1,16 @@
 #feed forward perceptron model
 
+import numpy as np
 
-inputs = [2.3, 4.3, 1.4]
-weights = [3.1, 1.2, 4.2]
-bias = 3
+inputs = [[1, 2, 3, 4], 
+            [1, 2, 3, 4], 
+            [1, 2, 3, 4]]
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+weights = [ [3.1, 1.2, 4.2, 5.1],
+            [1.2, 2.3, 3.4, 4.5],
+            [3.2, 5.4, 2.3, 1.2]]
+bias = [1, 1.5, 2]
+
+output = np.dot(inputs, np.array(weights).T) + bias
+
 print(output)
