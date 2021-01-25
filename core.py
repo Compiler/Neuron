81d9c6470f72f901d64e2e9f00da9e1442896122
compiler <lukerochedev@gmail.com>
2021-01-25 08:38:46 -0500

    layering feed forward

--- core.py
+++ core.py
@@ -1,16 +1,22 @@
 #feed forward perceptron model
 
 import numpy as np
+np.random.seed(0)
 
 inputs = [[1, 2, 3, 4], 
-            [1, 2, 3, 4], 
-            [1, 2, 3, 4]]
+            [4,3, 2, 1], 
+            [4, 2, 2, 4]]
 
-weights = [ [3.1, 1.2, 4.2, 5.1],
-            [1.2, 2.3, 3.4, 4.5],
-            [3.2, 5.4, 2.3, 1.2]]
-bias = [1, 1.5, 2]
+class Layer:
+    def __init__(self, inputCount, neuronCount):
+        self.weights = 0.1* np.random.randn(inputCount, neuronCount)
+        self.biases = 0.0001 * np.random.randn(1, neuronCount)
+        self.outputs = []
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
 
-output = np.dot(inputs, np.array(weights).T) + bias
-
-print(output)
+layer1 = Layer(4, 3)
+layer2 = Layer(3, 3)
+layer1.forward(inputs)
+layer2.forward(layer1.outputs)
+print(layer1.outputs)
